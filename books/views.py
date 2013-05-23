24445f1561c1ac9fc46dd978edc66685a76eacbe
Imran Shahid <narmi79@gmail.com>
2013-05-23 11:42:06 +1000

    Change the layout to remove the src folder and unwanted files.

--- books/views.py
+++ books/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import get_object_or_404, render, render_to_response
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-import string, random
-
-from books.models import Book, Word, WordCount
+import string
 from books.forms import BookUploadForm
+from books.models import Book, Word, WordCount
+
 
 def index(request):
     books = Book.objects.order_by('title')
     return render(request, 'books/index.html', {'books': books})
 
+
 def book(request, id):
     book = get_object_or_404(Book, pk=id)
     return render(request, 'books/book.html', {'book': book})
+
 
 def add(request):
     # Handle file upload
@@ -26,6 +28,7 @@ def add(request):
     else:
         form = BookUploadForm() # An empty, unbound form
         return render(request, 'books/add.html', {'form': form})
+
 
 def process_file(fp):
     book = read_gutenberg_headers(fp)
@@ -44,6 +47,7 @@ def process_file(fp):
 
         count = WordCount(book=book, word=word, count=hist[key])
         count.save()
+
 
 def read_gutenberg_headers(fp):
     book = Book()
