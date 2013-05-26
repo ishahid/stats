fc6fee6cdad925e451a52a2a700845766418b5da
Imran Shahid <narmi79@gmail.com>
2013-05-26 18:43:38 +1000

    Cosmetic improvements on the add book form including display of link to Project Gutenberg.

--- books/forms.py
+++ books/forms.py
@@ -1,7 +1,11 @@
 from django import forms
+from django.utils.safestring import mark_safe
+
 
 class BookUploadForm(forms.Form):
     txt_book = forms.FileField(
-                label = 'Select a file',
-                help_text = 'Make sure that it is a Project Gutenberg TXT book.'
+                label = 'Upload an ebook in TXT format...',
+                help_text = mark_safe('Make sure that it is a '
+                                      '<a target="_blank" href="http://www.gutenberg.org">Project Gutenberg</a>'
+                                      ' ebook in TXT format.')
              )
