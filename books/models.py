02215e6123d53d6996261f2b2dc5472ff413246d
Imran Shahid <narmi79@gmail.com>
2013-05-28 22:28:10 +1000

    Added common words, which are to be excluded from the cloud list at a later stage.

--- books/models.py
+++ books/models.py
@@ -37,6 +37,17 @@ class Book(models.Model):
     def get_word_cloud(self):
         """Returns word cloud from this book based upon the algorithm defined at wikipedia."""
         """http://en.wikipedia.org/wiki/Tag_cloud"""
+
+        excludes = ['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'i', 'it', 'for', 'not', 'on',
+                    'with', 'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we',
+                    'say', 'her', 'she', 'or', 'an', 'will', 'my', 'one', 'all', 'would', 'there', 'their', 'did'
+                    'what', 'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me', 'when', 'make',
+                    'can', 'like', 'time', 'no', 'just', 'him', 'know', 'take', 'people', 'into', 'year', 'your',
+                    'good', 'some', 'could', 'them','see', 'other', 'than', 'then', 'now', 'look', 'only', 'come',
+                    'its', 'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first',
+                    'well', 'way', 'even', 'new', 'want', 'because', 'any', 'these', 'give', 'day', 'most', 'us'
+                    'was', 'were']
+
         hist = {}
         list = self.get_most_common_words(100)
         for wc in list:
